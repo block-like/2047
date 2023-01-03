1dc3f2563c51f3f7e1df56ee47b654a121dd378c
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2023-01-03 11:39:23 +0000

    fixes

--- mod/data.js
+++ mod/data.js
@@ -1,6 +1,7 @@
 const modData = {
     name: "2047", //Name of 2048 mod
     styleLimit: 2047, //Limit where tile-super is used instead
+    winTile: 2047, //Tile which you win on
     tileFunction: (currentNumber) => { //Function to calculate next tile
         if (currentNumber == 1024) {
             return 2047
@@ -10,3 +11,9 @@ const modData = {
         return currentNumber*2
     }
 }
+
+//extra stuff
+
+document.title = modData.name
+document.getElementById('game-name') = modData.name
+document.getElementById('game-intro-tile') = `${modData.winTile.toString()} tile!`
